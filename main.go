350b1a71d820009f699de55a65820840c73b5de0
Reno DuBois <me@renodubois.com>
2022-12-16 13:13:18 -0600

    Convert bot to use Application Commands (slash commands)

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
 	"syscall"
 	"time"
 
@@ -16,7 +16,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const prefix string = "!"
+var RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 
 func check(e error) {
 	if e != nil {
@@ -24,19 +24,100 @@ func check(e error) {
 	}
 }
 
-func contains(source []string, target string) bool {
-	for _, str := range source {
-		if str == target {
-			return true
-		}
-	}
-	return false
-}
-
 type quoteRow struct {
 	ID        int
 	QuoteText string
 	DateAdded int
+}
+
+var commandsHaveDMPermission = false
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "quote",
+			Description: "Retrieves a random quote, or a specific quote",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "quote-id",
+					Description: "ID of specific quote",
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Required:    false,
+				},
+			},
+			DMPermission: &commandsHaveDMPermission,
+		},
+		{
+			Name:        "addquote",
+			Description: "Adds a new quote to the database",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "quote-text",
+					Description: "The text of the quote you want to enter",
+					Type:        discordgo.ApplicationCommandOptionString,
+					Required:    true,
+				},
+			},
+			DMPermission: &commandsHaveDMPermission,
+		},
+	}
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"quote": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			options := formatSlashCommandOptions(i.ApplicationCommandData().Options)
+			if quoteID, ok := options["quote-id"]; ok {
+				// Getting a specific quote
+				quote := getSpecificQuote(int(quoteID.IntValue()))
+				if quote.ID > 0 {
+					slashCommandTextRespond("> "+quote.QuoteText, s, i)
+				} else {
+					slashCommandTextRespond("Quote not found!", s, i)
+				}
+				return
+			}
+			quotes := getQuotes()
+			quote := quotes[rand.Intn(len(quotes))]
+			msg := "> " + quote.QuoteText + "\n Quote ID " + strconv.Itoa(quote.ID)
+			slashCommandTextRespond(msg, s, i)
+		},
+		"addquote": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			options := formatSlashCommandOptions(i.ApplicationCommandData().Options)
+			msg := "No quote text provided."
+			if quoteText, ok := options["quote-text"]; ok {
+				res := addQuote(getUsernameFromInteraction(i), quoteText.StringValue())
+				msg = "Something went wrong adding a quote - try again later."
+				if res > 0 {
+					msg = "New quote added! Quote ID: " + strconv.FormatInt(res, 10)
+				}
+			}
+			slashCommandTextRespond(msg, s, i)
+		},
+	}
+)
+
+func formatSlashCommandOptions(opts []*discordgo.ApplicationCommandInteractionDataOption) map[string]*discordgo.ApplicationCommandInteractionDataOption {
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(opts))
+	for _, opt := range opts {
+		optionMap[opt.Name] = opt
+	}
+	return optionMap
+}
+
+func slashCommandTextRespond(msg string, s *discordgo.Session, i *discordgo.InteractionCreate) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: msg,
+		},
+	})
+}
+
+func getUsernameFromInteraction(i *discordgo.InteractionCreate) string {
+	if i.Member != nil {
+		return i.Member.User.Username
+	}
+	if i.User != nil {
+		return i.User.Username
+	}
+	return ""
 }
 
 func getQuotes() []quoteRow {
@@ -101,95 +182,45 @@ func addQuote(username string, quote string) int64 {
 	return newQuoteId
 }
 
-func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.Author.ID == session.State.User.ID {
-		return
-	}
-
-	content := message.Content
-
-	if strings.HasPrefix(content, prefix) {
-		commands := []string{"addquote", "quote", "addedby"}
-		contentWithoutPrefix := strings.Replace(content, prefix, "", 1)
-		splitContent := strings.Split(contentWithoutPrefix, " ")
-		command := splitContent[0]
-		if contains(commands, command) {
-			splitContent = strings.SplitAfter(contentWithoutPrefix, command+" ")
-			// actually do the command
-			switch command {
-			case "addquote":
-				if len(splitContent) < 2 {
-					session.ChannelMessageSend(message.ChannelID, "No arguments provided. Usage: `addquote <quote>`")
-					return
-				}
-				args := splitContent[1]
-
-				name := message.Author.Username
-				result := addQuote(name, strings.ReplaceAll(args, "\n", " "))
-				if result > 0 {
-					session.ChannelMessageSend(message.ChannelID, "New quote added! Quote ID: "+strconv.FormatInt(result, 10))
-					return
-				} else {
-					session.ChannelMessageSend(message.ChannelID, "Failed to add quote. Try again, or yell at Reno.")
-				}
-				return
-			case "quote":
-				msg := ""
-				var quote quoteRow
-				if len(splitContent) < 2 {
-					// get a random quote
-					quotes := getQuotes()
-					quote = quotes[rand.Intn(len(quotes))]
-					msg = "> " + quote.QuoteText
-					msg += "\n Quote ID " + strconv.Itoa(quote.ID)
-					// TODO: add a verbose flag that displays this
-					// if quote.DateAdded > 0 {
-					//	msg += " added on " + strconv.Itoa(quote.DateAdded)
-					//}
-
-				} else {
-					args := splitContent[1]
-					quoteID, err := strconv.Atoi(args)
-					if err != nil {
-						quoteID = 0
-					}
-					quote = getSpecificQuote(quoteID)
-					if quote.ID <= 0 {
-						msg = "Quote not found!"
-					} else {
-						msg = "> " + quote.QuoteText
-					}
-				}
-
-				session.ChannelMessageSend(message.ChannelID, msg)
-				return
-			}
-			session.ChannelMessageSend(message.ChannelID, message.Content)
-		}
+func interactionHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if handler, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+		handler(s, i)
 	}
 }
 
 func main() {
-	// Get the bot's OAuth token
-	data, err := os.ReadFile("./.token")
-	check(err)
-	token := string(data)
-	token = strings.Trim(token, "\n")
+	token := os.Getenv("QB_OAUTH_TOKEN")
+	if len(token) <= 0 {
+		fmt.Println("The environment variable QB_OAUTH_TOKEN is required to be set before running the bot.")
+		os.Exit(1)
+	}
 
-	discord, err := discordgo.New("Bot " + token)
-	check(err)
-
-	discord.AddHandler(messageCreate)
-
-	err = discord.Open()
+	s, err := discordgo.New("Bot " + token)
 	check(err)
 
-	fmt.Println("Bot running. CTRL-C to stop.")
+	s.AddHandler(interactionHandler)
+
+	err = s.Open()
+	check(err)
+
+	log.Println("Bot running. CTRL-C to stop.")
+
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
+		if err != nil {
+			registeredCommands[i] = cmd
+		}
+	}
+
 	// create a channel that looks for system signals (that has a size of one event)
 	osChannel := make(chan os.Signal, 1)
 	signal.Notify(osChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	// TODO: what the heck does this do
+	// This flushes the value inside of osChannel, if it has one - ensures that
+	// the channel will have room to continue processing signals as they come in.
+	// In this case, we're using signal.Notify to catch SIGINT, SIGTERM & Interrupt
+	// signals, and we redirect them out to the program.
 	<-osChannel
 
-	discord.Close()
+	s.Close()
 }
